34aaa411a36e9aed9bd827d2ca525ca68a9da2be
pedro-dev15 <pedrocpereira.67@gmail.com>
2025-12-13 21:49:34 -0300

    Server running

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import * as Task from "./task.controller.js"
+
+const router = Router()
+
+//Rota padrão
+router.get("/", Task.none)
+//Criação de uma task
+router.post("/tasks", Task.create)
+//Pegar todas as tasks
+router.get("/tasks", Task.findAll)
+//Pegar só uma task
+router.get("/tasks/:id", Task.findOne)
+//Alteração de uma task
+router.put("/tasks/:id", Task.update)
+//Deletando uma task
+router.delete("/tasks/:id", Task.remove)
+
+export default router;
